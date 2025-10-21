9f7bbdd7ba971c230a054774bcc40312b16b5181
root <root@ip-10-0-8-16.ap-northeast-1.compute.internal>
2025-10-21 11:50:08 +0000

    post_list and post_create追加

--- handson_app/views.py
+++ handson_app/views.py
@@ -1,5 +1,20 @@
-from django.views.generic import TemplateView
+from django.views import generic
+from .models import Post # Postモデルをimport
+from django.urls import reverse_lazy
+from .forms import PostCreateForm 
 
 # Create your views here.
-class IndexView(TemplateView):
+class IndexView(generic.TemplateView):
     template_name = 'handson_app/index.html'
+
+class PostListView(generic.ListView): # generic の ListViewクラスを継承
+    model = Post # 一覧表示させたいモデルを呼び出し
+    template_name = 'handson_app/post_list.html'
+
+class PostCreateView(generic.CreateView): # 追加
+    model = Post # 作成したい model を指定
+    form_class = PostCreateForm # 作成した form クラスを指定
+    success_url = reverse_lazy('handson_app:post_list')
+    template_name = 'handson_app/post_create.html'
+
+
